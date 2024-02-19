98ce2f50734110165508fccdd1c4b6b9c8826da1
roybrockington <r.brockington@gmail.com>
2024-02-19 16:10:42 +0000

    Autocomplete disable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
             <div className="z-10 w-5xl w-full items-center justify-between font-mono text-sm">
-                <form onSubmit={submitSearch} className="flex items-center">
+                <form autoComplete="off" onSubmit={submitSearch} className="flex items-center">
                     <input type="search" value={search} name="search" id="search"
                         autoFocus
                         onChange={e=> updateSearch(e)}
